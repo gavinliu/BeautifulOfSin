c6fd777fdc9485eb14fc6ae946ccfc6b52adb672
刘云龙 <gavinliu@meizu.com>
2015-03-30 19:42:39 +0800

    update WaveView

--- src/main/java/cn/gavinliu/beautifulofsin/WaveView.java
+++ src/main/java/cn/gavinliu/beautifulofsin/WaveView.java
@@ -2,13 +2,24 @@ package cn.gavinliu.beautifulofsin;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.DrawFilter;
+import android.graphics.Paint;
+import android.graphics.PaintFlagsDrawFilter;
 import android.util.AttributeSet;
 import android.view.View;
 
-/**
- * Created by Gavin on 15/3/29.
- */
 public class WaveView extends View {
+
+    private static final int X_SPEED = 20;
+    private int mXOffset = 0;
+
+    private int mViewWidth, mViewHeight;
+
+    private Paint mPaint;
+    private DrawFilter mDrawFilter;
+
+    private float[] mPointY;
+    private float[] mDynamicPointY;
 
     public WaveView(Context context) {
         super(context);
@@ -20,16 +31,55 @@ public class WaveView extends View {
 
     public WaveView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setStyle(Paint.Style.FILL);
+
+        mDrawFilter = new PaintFlagsDrawFilter(0, Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        canvas.setDrawFilter(mDrawFilter);
+
+        runWave();
+
+        for (int i = 0; i < mViewWidth; i++) {
+            canvas.drawLine(i, mViewWidth - mDynamicPointY[i] - 400, i, mViewHeight, mPaint);
+        }
+
+        mXOffset += X_SPEED;
+
+        if (mXOffset > mViewWidth) {
+            mXOffset = 0;
+        }
+
+        postInvalidate();
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+
+        mViewWidth = w;
+        mViewHeight = h;
+
+        mPointY = new float[w];
+        mDynamicPointY = new float[w];
+
+        // y = a * sin(2π) + b
+        for (int i = 0; i < w; i++) {
+            // i/w, 把sin(2π)拉升到整个View宽度
+            mPointY[i] = (float) (30 * Math.sin(2 * Math.PI * i / w));
+        }
+    }
+
+    private void runWave() {
+        int yInterval = mPointY.length - mXOffset;
+        System.arraycopy(mPointY, 0, mDynamicPointY, mXOffset, yInterval);
+        System.arraycopy(mPointY, yInterval, mDynamicPointY, 0, mXOffset);
     }
 
 }
